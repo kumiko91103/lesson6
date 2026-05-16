89ed806aa5535f03b6014a5295c4e5cfaa2f8809
sunny <kumiko91103@gmail.com>
2026-05-16 23:28:56 +0800

    Initial Commit

---
+++ lesson6/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lesson6
+//
+//  Created by 114-2Workshop17 on 2026/5/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lesson6/lesson6App.swift
@@ -0,0 +1,17 @@
+//
+//  lesson6App.swift
+//  lesson6
+//
+//  Created by 114-2Workshop17 on 2026/5/16.
+//
+
+import SwiftUI
+
+@main
+struct lesson6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
